282fb6679c0beab4430c3f10f5a6a02cfa3288cc
gundam1993 <gundam1993@gmail.com>
2016-09-25 22:21:12 +0800

    working on slide feature

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,9 +1,15 @@
 <template>
   <div id="slide">
-      <div v-for="data in content" class="slide-box">
+    <div class="slide-container">
+      <div v-for="data in content" 
+           class="slide-box" 
+           @touchstart="setTouchX()" 
+           @touchmove="touchMove()"
+           @touchend="touchEnd()">
         <img :src="data.image" alt="">
         <h3>{{ data.title }}</h3>
       </div>
+    </div>
     <div class="slide-pointers">
       <div v-for="data in content" class="slide-ponit"></div>
       <div id="slide-ponit-display"></div>
@@ -13,6 +19,12 @@
 
 <script>
   export default {
+    data() {
+      return {
+        touchX : 0,
+        index: 0,
+      }
+    },
     props: {
       content: {
         type: Array,
@@ -22,26 +34,50 @@
       },
     },
     methods: {
-      slideMove() {
+      setTouchX() {
+        this.touchX = event.touches[0].clientX;
+      },
 
+      touchMove() {
+        event.preventDefault();
+        event.target.parentNode.parentNode.style.left =  (event.touches[0].clientX  - this.touchX) + 'px';
+      },
+      slideMove(i) {
+        
       }
-    }
+      touchEnd() {
+        var distanse = event.target.parentNode.parentNode.style.left
+        console.log(event.target.parentNode.parentNode.style.left);
+        if (Math.abs(event.target.x) <= event.target.offsetWidth / 2) {
+          
+        }else{
+          
+        }
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   #slide {
-    width: 100%;
     height: 13rem;
     position: relative;
 
-    .slide-box {
-      width: 100%;;
-      height: 13rem;
-      z-index: 1;
+    .slide-container {
+      width: 500%;
+      height: 100%;
+      display: flex;
+      flex-direction: row-reverse;
       position: absolute;
       top: 0;
       left: 0;
+    }
+
+    .slide-box {
+      width: 20%;
+      height: 13rem;
+      z-index: 1;
+      position: relative;
 
       img {
         width: 100%;
